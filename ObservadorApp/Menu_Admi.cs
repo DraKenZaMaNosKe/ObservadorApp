f59d792174db5344a937d017d5c9c9cb4f181d94
Jorge <serpifox@gmail.com>
2018-10-21 19:42:37 -0500

    Menu observador

--- ObservadorApp/Menu_Admi.cs
+++ ObservadorApp/Menu_Admi.cs
@@ -16,5 +16,61 @@ namespace ObservadorApp
         {
             InitializeComponent();
         }
+
+        Observacion_admi observacion_Admi = null;
+        Registro_Observador_Admi registro_Admi = null;
+        Zona_Admi zona = null;
+        Especie_Admi especie = null;
+        Asociacion_Admi asociacion = null;
+
+        private void btnObservador_Click(object sender, EventArgs e)
+        {
+            if(registro_Admi == null)
+            {
+                registro_Admi = new Registro_Observador_Admi();
+                this.Hide();
+                registro_Admi.Show();
+            }
+        }
+
+        private void btnZona_Click(object sender, EventArgs e)
+        {
+            if(zona == null)
+            {
+                zona = new Zona_Admi();
+                this.Hide();
+                zona.Show();
+            }
+        }
+
+        private void btnEspecie_Click(object sender, EventArgs e)
+        {
+            if(especie == null)
+            {
+                especie = new Especie_Admi();
+                this.Hide();
+                especie.Show();
+            }
+        }
+
+        private void btnAsociacion_Click(object sender, EventArgs e)
+        {
+            if(asociacion == null)
+            {
+                asociacion = new Asociacion_Admi();
+                this.Hide();
+                asociacion.Show();
+            }
+        }
+
+        private void btnObservacion_Click(object sender, EventArgs e)
+        {
+            if(observacion_Admi == null)
+            {
+                observacion_Admi = new Observacion_admi();
+                this.Hide();
+                observacion_Admi.Show();
+            }
+        }
     }
 }
