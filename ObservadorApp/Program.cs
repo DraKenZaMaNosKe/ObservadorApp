b9c7be6c01cf7f1139aaef57c0e76b44cae10989
manuel estrada ramirez <Manuel.Estrada.Ramirez@hotmail.com>
2018-10-21 20:26:22 -0500

    splash terminado

--- ObservadorApp/Program.cs
+++ ObservadorApp/Program.cs
@@ -16,7 +16,7 @@ namespace ObservadorApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new Inicio_sesion());
         }
     }
 }
